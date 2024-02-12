67f705eb60eec25ed33b2a90a2a0a45e4bed44a9
UhaiMong <uhaimarma2016@gmail.com>
2024-02-12 19:49:51 +0600

    wip

--- account/views.py
+++ account/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import UserRegistrationForm, UserUpdateForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.views import PasswordChangeView as DjangoPasswordChangeView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
 from django.shortcuts import redirect
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 class UserRegistrationView(FormView):
@@ -46,3 +49,22 @@ class UserBankAccountUpdateView(View):
             form.save()
             return redirect('profile')
         return render(request, self.template_name, {'form': form})
+
+
+class PasswordChangeView(DjangoPasswordChangeView):
+    template_name = 'password_change.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.request.user
+        subject = "Password Changed"
+        message_template = 'password_change_email.html'  # Your email template path
+        self.send_password_change_email(user, subject, message_template)
+        return response
+
+    def send_password_change_email(self, user, subject, template):
+        message = render_to_string(template, {'user': user})
+        send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+        send_email.attach_alternative(message, 'text/html')
+        send_email.send()
